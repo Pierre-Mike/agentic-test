b5e63f195295a612607bf64b231e0cee74648614
Pierre-Mike <pm.lemeliner@gmail.com>
2026-01-29 19:46:12 -0800

    Revert "feat: Add greet function with name parameter (#47)" (#48)
    This reverts commit 770ae37f82a9630ac1d05b53095433e20acc79a2.

--- shared/src/utils/greet.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns a personalized greeting message.
- * @param name - The name to greet
- * @returns A greeting string in the format "Hello, {name}!"
- */
-export function greet(name: string): string {
-	return `Hello, ${name}!`;
-}
